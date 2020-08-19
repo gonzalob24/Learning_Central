3231b1e37d7ef8251429f6c49aaec0d8891b8d73
Gonzalo Betancourt <gonzalobetancourt@me.com>
2020-08-18 19:18:36 -0500

    code review

--- Machine_Learning/Regression/multiple_linear_regression.py
+++ Machine_Learning/Regression/multiple_linear_regression.py
@@ -55,8 +55,8 @@ regressor.fit(x_train, y_train)
 # Predicting the Test set results
 y_pred = regressor.predict(x_test)
 np.set_printoptions(precision=2)
-# print(np.concatenate((y_pred.reshape(-1,1), y_test.reshape(-1,1))))
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1))))
+# print(np.concatenate((y_pred.reshape(-1,1), y_test.reshape(-1,1)),1))
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)), 1))
 
 
 # Building the optimal model using Backward Elimination
