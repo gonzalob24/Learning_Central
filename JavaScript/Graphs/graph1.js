da014e2fa3256637aeb61b65fb739b1eab79c389
Gonzalo Betancourt <gonzalo@localhost.localdomain>
2020-12-03 00:48:28 -0500

    graphs review

--- JavaScript/Graphs/graph1.js
+++ JavaScript/Graphs/graph1.js
@@ -1,88 +1,110 @@
 "use strict";
 
 class Graph {
-	constructor() {
-		this.adjacencyList = {};
-	}
+  constructor() {
+    this.adjacencyList = {};
+  }
 
-	addVertex(vertex) {
-		if (!this.adjacencyList[vertex]) {
-			this.adjacencyList[vertex] = [];
-		}
-	}
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
 
-	addEdge(vertex1, vertex2) {
-		if (this.adjacencyList[vertex1]) {
-			this.adjacencyList[vertex1].push(vertex2);
-			this.adjacencyList[vertex2].push(vertex1);
-		}
-	}
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+    }
+  }
 
-	removeEdge(vertex1, vertex2) {
-		this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
-			(vertex) => vertex !== vertex2
-		);
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (vertex) => vertex !== vertex2
+    );
 
-		this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
-			(vertex) => vertex !== vertex1
-		);
-	}
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (vertex) => vertex !== vertex1
+    );
+  }
 
-	removeVertex(vertex) {
-		const del_vertex = this.adjacencyList[vertex];
-		del_vertex.forEach((city) => this.removeEdge(vertex, city));
-		delete this.adjacencyList[vertex];
+  removeVertex(vertex) {
+    const del_vertex = this.adjacencyList[vertex];
+    del_vertex.forEach((city) => this.removeEdge(vertex, city));
+    delete this.adjacencyList[vertex];
 
-		// while (this.adjacencyList[vertex].length) {
-		// 	const del_vertex = this.adjacencyList[vertex].pop();
-		// 	this.removeEdge(vertex, del_vertex);
-		// }
-	}
+    // while (this.adjacencyList[vertex].length) {
+    // 	const del_vertex = this.adjacencyList[vertex].pop();
+    // 	this.removeEdge(vertex, del_vertex);
+    // }
+  }
 
-	dfsRecursive(start) {
-		const endResult = [];
-		const visited = {};
-		// Store adjacency list in variable becasue inside of the IIFE "this" will be refering to IEFF
-		const adjacencyList = this.adjacencyList;
-		(function dfs(vertex) {
-			if (!vertex) {
-				return null;
-			}
-			visited[vertex] = true;
-			endResult.push(vertex);
-			adjacencyList[vertex].forEach((neighbor) => {
-				if (!visited[neighbor]) {
-					dfs(neighbor);
-				}
-			});
-		})(start); // invoke it with start
+  dfsRecursive(start) {
+    const endResult = [];
+    const visited = {};
+    // Store adjacency list in variable becasue inside of the IIFE "this" will be refering to IEFF
+    const adjacencyList = this.adjacencyList;
+    (function dfs(vertex) {
+      if (!vertex) {
+        return null;
+      }
+      visited[vertex] = true;
+      endResult.push(vertex);
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          dfs(neighbor);
+        }
+      });
+    })(start); // invoke it with start
 
-		return endResult;
-	}
+    return endResult;
+  }
 
-	dfsIterative(start) {
-		const stack_arr = [];
-		stack_arr.push(start);
-		const results = [];
-		const visited = {}; // set visited vertex to true
+  dfsIterative(start) {
+    const stack_arr = [];
+    stack_arr.push(start);
+    const results = [];
+    const visited = {}; // set visited vertex to true
 
-		let vertex;
-		visited[start] = true;
-		// as long as stack is not empty
-		while (stack_arr.length !== 0) {
-			vertex = stack_arr.pop();
-			results.push(vertex);
+    let vertex;
+    visited[start] = true;
+    // as long as stack is not empty
+    while (stack_arr.length !== 0) {
+      vertex = stack_arr.pop();
+      results.push(vertex);
 
-			this.adjacencyList[vertex].forEach((neighbor) => {
-				if (!visited[neighbor]) {
-					visited[neighbor] = true;
-					stack_arr.push(neighbor);
-				}
-			});
-		}
+      this.adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack_arr.push(neighbor);
+        }
+      });
+    }
 
-		return results;
-	}
+    return results;
+  }
+
+  bfs(start) {
+    const queue = [];
+    const results = [];
+    const visited = {};
+
+    visited[start] = true;
+    queue.push(start);
+    var front_node;
+
+    while (queue.length != 0) {
+      front_node = queue.shift();
+      results.push(front_node);
+      this.adjacencyList[front_node].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return results;
+  }
 }
 
 const g = new Graph();
