9f410fd0fe18baaefab63a3b8e9d9e17146a6558
Gonzalo Betancourt <gonzalobetancourt@me.com>
2020-12-08 09:41:35 -0600

    Dynamic Problem

---
+++ Python_Programming/Dynamic/canSum.py
@@ -0,0 +1,42 @@
+def canSum(targetSum, nums, memo={}):
+    # which argument directly impacts the return value. nums does not change, targetSum changes.
+    if targetSum in memo:
+        return memo[targetSum]
+    # base case when to stop looking for sum
+    if targetSum == 0:
+        return True
+
+    if targetSum < 0:
+        return False
+
+    for num in nums:
+        remainder = targetSum - num
+        if canSum(remainder, nums, memo) == True:
+            memo[targetSum] = True
+            return True
+    memo[targetSum] = False
+    return False
+
+
+def canSum1(targetSum, nums):
+    # which argument directly impacts the return value. nums does not change, targetSum changes.
+
+    # base case when to stop looking for sum
+    if targetSum == 0:
+        return True
+
+    if targetSum < 0:
+        return False
+
+    for num in nums:
+        remainder = targetSum - num
+        if canSum(remainder, nums) == True:
+            return True
+    return False
+
+
+print(canSum1(7, [2, 3]))
+print(canSum1(7, [5, 3, 4, 7]))
+print(canSum1(7, [2, 4]))
+print(canSum1(8, [2, 3, 5]))
+# print(canSum1(300, [7, 14]))
---
+++ Python_Programming/Dynamic/howSum.py
@@ -0,0 +1,31 @@
+def howSum(targetSum, nums, memo={}):
+    if targetSum in memo:
+        return memo[targetSum]
+
+    if targetSum == 0:
+        return []
+
+    if targetSum < 0:
+        return None
+
+    for num in nums:
+        remainder = targetSum - num
+        # howSum may return [] or None
+        remainderResult = howSum(remainder, nums, memo)
+        if remainderResult != None:
+            memo[targetSum] = [*remainderResult, num]
+            return memo[targetSum]
+
+    memo[targetSum] = None
+    return None
+
+
+# arr = [1, [2, 3], 4, 5]
+# print(*arr)
+
+
+print(howSum(7, [2, 3]))
+print(howSum(7, [5, 3, 4, 7]))
+print(howSum(7, [2, 4]))
+print(howSum(8, [2, 3, 5]))
+print(howSum(300, [7, 14]))
