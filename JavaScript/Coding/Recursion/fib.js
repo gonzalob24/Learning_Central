b9a976234aa05ea5c0b40c3e8e27f63aa2619d36
Gonzalo Betancourt <gonzalobetancourt@me.com>
2020-12-09 21:51:39 -0600

    Dynamic Problems

--- JavaScript/Coding/Recursion/fib.js
+++ JavaScript/Coding/Recursion/fib.js
@@ -1,17 +1,32 @@
 function fibonacci(n) {
-    // first two numbers ar 0 and 1
-    var first = 0;
-    var second = 1;
+  // first two numbers ar 0 and 1
+  var first = 0;
+  var second = 1;
 
-    if (n < 1) {
-        return "n can't be less than 1"
-    } else if (n === 1 || n === 2) {
-        return n - 1;
-    } else {
-        return fibonacci(n - 1) + fibonacci(n - 2)
-    }
+  if (n < 1) {
+    return "n can't be less than 1";
+  } else if (n === 1 || n === 2) {
+    return n - 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
 }
 
-// 0 1 1 2 3 
-console.log(fibonacci(5))
-console.log(fibonacci(10))
+function canConstruct(target, wordBank) {
+  if (target == "") {
+    return true;
+  }
+  for (let word of wordBank)
+    if (target.indexOf(word) == 0) {
+      suffix = target.slice(word.length);
+      if (canConstruct(suffix, wordBank) == true) {
+        return true;
+      }
+    }
+
+  return false;
+}
+// 0 1 1 2 3
+// console.log(fibonacci(5))
+// console.log(fibonacci(10))
+console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
