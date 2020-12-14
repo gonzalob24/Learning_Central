a7137ff9dd1d6345b9fa5bb223df004c2e9487b3
Gonzalo Betancourt <gonzalobetancourt@me.com>
2020-12-14 17:38:43 -0600

    leetcode practice

--- Python_Programming/Dynamic/howSum.py
+++ Python_Programming/Dynamic/howSum.py
@@ -1,4 +1,9 @@
+import math
+import collections
+
 # Combinatoric Problem
+
+
 def howSum(targetSum, nums, memo={}):
     if targetSum in memo:
         return memo[targetSum]
@@ -31,13 +36,47 @@ def howSum(targetSum, nums, memo={}):
 # print(howSum(8, [2, 3, 5], {}))
 # print(howSum(300, [7, 14], {}))
 arr = [1, 2, 3]
-print(arr)
 
 arr[:] = []
-print(arr)
 n = []
 a = [1, 2, 3]
-n[:] = a[:]
-n[0] = 'H'
-print(n)
-print(10 + None)
+# n[:] = a[:]
+# n[0] = 'H'
+p = [8, 4, 6, 2, 3]
+
+
+def finalPrices(prices):
+    stk = []
+    for i, p in enumerate(prices):
+        while stk and prices[stk[-1]] >= p:
+            prices[stk.pop()] -= p
+        stk.append(i)
+    return prices
+
+
+# print(finalPrices([8, 4, 6, 2, 3]))
+# print('True or false')
+# print(n and (p[n[-1]] >= 8))
+# print(p[n.pop()])
+moves = 'UUUDDDLLL'
+c = collections.Counter(moves)
+print(c.items())
+# for k, v in c.items():
+#     print(k, v)
+print(round(4.42))
+print(min(1, 2))
+
+ar = [[1], [2, 3]]
+print(ar.index([1]))
+name = "Gonzalo"
+print(name.find("w"))
+ar2 = [[1], [2, 3, 4]]
+ar3 = [1, 2, 3, 4]
+for n in ar2:
+    print(ar3[1:len(n)] == n)
+art = [1, 2, 3, 4]
+art2 = [6, 2, 6, 5, 1, 2]
+art2.sort()
+print(art[::2])
+print(art2[::2])
+print((art2[::2]))
--- Python_Programming/README.md
+++ Python_Programming/README.md
@@ -9,10 +9,15 @@ I may not do all of the problems from each chapter since a lot of it is just for
     - copy.copy(A) vs copy.deepcopy(A): Use import copy
         - Relevant for compund objects
     - I can also use slicing to copy arrays
+    - think about storing elements in a stack or queue for temporary storage
+    - Once I have a brute force solution. Look into time complexity and determine why it is slow. what is impacting its performance. Time/Space complexity.
 
 ## Trees
 
-    - understand tree traversal using. Recursion makes it easier to travers trees.
+    - understand tree traversal: Inorder, Preorder, Postorder, Level Order
+    - Recursion makes it easier to travers trees.
+    - Try to use helper functions to make traversal easier
+    -
 
 ## Recursion
 
@@ -35,3 +40,10 @@ I may not do all of the problems from each chapter since a lot of it is just for
     - Think about what goes inside each node
     - What are my branch factors. How do I get from one node to the next
     - The leafs will be my base cases.
+
+## Collections
+
+    - Math
+    - max, sum can be used on array
+    - name.count(char) --> counts cahracters in a string
+    - collections.count(object) --> create dictionary very easily
