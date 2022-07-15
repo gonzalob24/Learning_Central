c80435427977213007cce16e6a6b2be48ba8c473
Gonzalo Betancourt <gonzalobetancourt@me.com>
2022-07-15 17:41:23 -0400

    java basics review

---
+++ Java_Programming/JavaIn28Minutes/One/Intro/Multiplication.java
@@ -0,0 +1,9 @@
+package One.Intro;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        System.out.println("Up and running");
+        System.out.println("Up and running".charAt(1));
+        System.out.println("Up and running".substring(1, "Up and running".length())); // not including the end
+    }
+}
---
+++ Java_Programming/JavaIn28Minutes/One/Intro/ScannerExample.java
@@ -0,0 +1,11 @@
+package One.Intro;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int n = console.nextInt();
+        System.out.println("number is: " + n);
+    }
+}
