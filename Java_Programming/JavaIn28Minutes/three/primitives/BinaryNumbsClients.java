315f983e5245eb119f68ff6d1e2d0940b7194821
Gonzalo Betancourt <gonzalobetancourt@me.com>
2022-07-20 17:54:35 -0400

    primitive data

---
+++ Java_Programming/JavaIn28Minutes/three/primitives/BinaryNumbsClients.java
@@ -0,0 +1,16 @@
+package three.primitives;
+
+import java.math.BigDecimal;
+
+public class BinaryNumbsClients {
+    public static void main(String[] args) {
+        BinaryNum num1 = new BinaryNum(1,2);
+        System.out.println(num1.add());
+
+        BigDecimal d1 = new BigDecimal(2);
+        System.out.println(d1);
+
+        char ch = 'a';
+        System.out.println(ch);
+    }
+}
