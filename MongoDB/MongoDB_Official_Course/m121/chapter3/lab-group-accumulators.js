cf01c76cf99e0cdcbbcdd714e021c328b757ce44
Gonzalo Betancourt <gonzalobetancourt@me.com>
2022-08-30 22:08:28 -0400

    ch 3 review

--- MongoDB/MongoDB_Official_Course/m121/chapter3/lab-group-accumulators.js
+++ MongoDB/MongoDB_Official_Course/m121/chapter3/lab-group-accumulators.js
@@ -1,28 +1,27 @@
 /**
- * For all films that won at least 1 Oscar, calculate the standard deviation, highest, 
+ * For all films that won at least 1 Oscar, calculate the standard deviation, highest,
  * lowest, and average imdb.rating. Use the sample standard deviation expression.
- * 
- * HINT - All movies in the collection that won an Oscar begin with a string resembling 
+ *
+ * HINT - All movies in the collection that won an Oscar begin with a string resembling
  * one of the following in their awards field
  */
 
 // $elemMatch is used to see if an array exists
 
 db.movies.aggregate([
-    {
-        $match: {
-            awards: { $exists: true },
-            awards: /Won \d{1,2} Oscars?/
-        }
-    },
-    {   
-        $group: {
-            _id: 0,
-            "imdb_rating_min": {$min: "$imdb.rating"},
-            "imdb_rating_max": {$max: "$imdb.rating"},
-            "imdb_rating_avg": {$avg: "$imdb.rating"},
-            "imdb_rating_stdve": {$stdDevSamp: "$imdb.rating"}
-
-        }
-    }
-])
+	{
+		$match: {
+			awards: { $exists: true },
+			awards: /Won \d{1,2} Oscars?/,
+		},
+	},
+	{
+		$group: {
+			_id: null,
+			imdb_rating_min: { $min: "$imdb.rating" },
+			imdb_rating_max: { $max: "$imdb.rating" },
+			imdb_rating_avg: { $avg: "$imdb.rating" },
+			imdb_rating_stdve: { $stdDevSamp: "$imdb.rating" },
+		},
+	},
+]);
--- MongoDB/MongoDB_Official_Course/m121/chapter3/lecture-exaamples.js
+++ MongoDB/MongoDB_Official_Course/m121/chapter3/lecture-exaamples.js
@@ -1,60 +1,133 @@
-db.icecream_data.aggregate([{
-    $project: {
-        _id: 0,
-        max_high: {
-            $reduce: {
-                input: "$trends",
-                initialValue: -Infinity,
-                in: {
-                    $cond: [
-                        {$gt: ["$$this.avg_high_tmp", "$$value"]},
-                        "$$this.avg_high_tmp",
-                        "$$value"
-                    ]
-                }
-            }
-        }
-    }
-}])
+db.movies.aggregate([
+	{
+		$group: { _id: "$year", num_films_in_year: { $sum: 1 } },
+	},
+	{
+		$sort: { num_films_in_year: -1 },
+	},
+]);
+
+db.movies.aggregate([
+	{
+		$group: {
+			_id: {
+				numDirectors: {
+					$cond: [{ $isArray: "$directors" }, { $size: "$directors" }, 0],
+				},
+			},
+			numFilms: { $sum: 1 }, // the number of documents that match
+			averageMetacritic: { $avg: "$metacritic" },
+		},
+	},
+	{
+		$sort: { "_id.numDirectors": -1 },
+	},
+]);
+
+db.movies.aggregate([
+	{
+		$match: { metacritic: { $gte: 0 } },
+	},
+	{
+		$group: { _id: null, averageMetacritic: { $avg: "$metacritic" } },
+	},
+]);
+
+db.movies.aggregate([
+	{
+		$group: {
+			_id: null,
+			count: { $sum: 1 },
+		},
+	},
+]);
+
+db.icecream_data.aggregate([
+	{
+		$project: {
+			_id: 0,
+			max_high: {
+				$reduce: {
+					input: "$trends",
+					initialValue: -Infinity,
+					in: {
+						$cond: [
+							{ $gt: ["$$this.avg_high_tmp", "$$value"] },
+							"$$this.avg_high_tmp",
+							"$$value",
+						],
+					},
+				},
+			},
+		},
+	},
+]);
+
+db.icecream_data.aggregate([
+	{
+		$project: {
+			_id: 0,
+			max_low: {
+				$reduce: {
+					input: "$trends",
+					initialValue: Infinity,
+					in: {
+						$cond: [
+							{ $lt: ["$$this.avg_low_tmp", "$$value"] },
+							"$$this.avg_low_tmp",
+							"$$value",
+						],
+					},
+				},
+			},
+		},
+	},
+]);
 
 // another way to achieve the same results
-db.icecream_data.aggregate([{
-    $project: {
-        _id: 0,
-        max_high: { $max: "$trends.avg_high_tmp"}
-    }
-}])
+db.icecream_data.aggregate([
+	{
+		$project: {
+			_id: 0,
+			max_high: { $max: "$trends.avg_high_tmp" },
+		},
+	},
+]);
 
-db.icecream_data.aggregate([{
-    $project: {
-        _id: 0,
-        max_low: { $min: "$trends.avg_low_tmp"}
-    }
-}]);
+db.icecream_data.aggregate([
+	{
+		$project: {
+			_id: 0,
+			max_low: { $min: "$trends.avg_low_tmp" },
+		},
+	},
+]);
 
-db.icecream_data.aggregate([{
-    $project: {
-        _id: 0,
-        avg_cpi: {$avg: "$trends.icecream_cpi"},
-        cpi_deviation: {$stdDevPop: "$trends.icecream_cpi"}
-    }
-}]);
+db.icecream_data.aggregate([
+	{
+		$project: {
+			_id: 0,
+			avg_cpi: { $avg: "$trends.icecream_cpi" },
+			cpi_deviation: { $stdDevPop: "$trends.icecream_cpi" },
+		},
+	},
+]);
 
-db.icecream_data.aggregate([{
-    $project: {
-        _id: 0,
-        "Yearly sales (millions)": {$sum: "$trends.icecream_sales_in_millions"}
-    }
-}])
+db.icecream_data.aggregate([
+	{
+		$project: {
+			_id: 0,
+			"Yearly sales (millions)": { $sum: "$trends.icecream_sales_in_millions" },
+		},
+	},
+]);
 
 // how many films have awards
 db.movies.aggregate([
-    {
-        $match: {
-            awards: { $exists: true}
-        }
-    },
-    {
-        $count: "awards"
-    }
-])
+	{
+		$match: {
+			awards: { $exists: true },
+			awards: /Won \d{1,2} Oscars?/,
+		},
+	},
+]);
