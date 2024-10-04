b94e25ff068754531c378d3325ef089f48c252ad
Gonzalo <gonzalobetancourt@me.com>
2024-10-04 17:05:00 -0500

    bfs and dfs review

--- JavaScript/ds_24/10_trees.js
+++ JavaScript/ds_24/10_trees.js
@@ -20,20 +20,20 @@ class BST {
 		}
 
 		let current_node = this.root;
-		while (current_node !== null) {
-			if (new_node.value > current_node.value) {
-				if (current_node.right !== null) {
-					current_node = current_node.right;
-				} else {
-					current_node.right = new_node;
-					return this;
-				}
-			} else if (new_node.value < current_node.value) {
-				if (current_node.left !== null) {
-					current_node = current_node.left;
-				} else {
+		while (true) {
+			if (current_node.value > value) {
+				if (!current_node.left) {
 					current_node.left = new_node;
 					return this;
+				} else {
+					current_node = current_node.left;
+				}
+			} else {
+				if (!current_node.right) {
+					current_node.right = new_node;
+					return this;
+				} else {
+					current_node = current_node.right;
 				}
 			}
 		}
@@ -140,6 +140,114 @@ class BST {
 			}
 		}
 	}
+
+	bfs() {
+		let current_node = this.root;
+		let list = [];
+		// add root node to queue
+		let queue = []; // keep track of children
+		queue.push(current_node);
+		while (queue.length > 0) {
+			current_node = queue.shift(); // get first item from the queue
+			// console.log(current_node.value);
+			list.push(current_node.value); // add value to the list
+			if (current_node.left) {
+				queue.push(current_node.left); // add left children to queue
+			}
+			if (current_node.right) {
+				queue.push(current_node.right); // add right children
+			}
+		}
+		return list;
+	}
+
+	bfs_recursive(queue, list) {
+		// base case
+		if (queue.length === 0) {
+			return list;
+		}
+		let current_node = queue.shift();
+		list.push(current_node.value);
+		if (current_node.left) {
+			queue.push(current_node.left); // add left children to queue
+		}
+		if (current_node.right) {
+			queue.push(current_node.right); // add right children
+		}
+		return this.bfs_recursive(queue, list);
+	}
+
+	// use recursion
+	dfs_in_order() {
+		return this.traverse_in_order(this.root, []);
+	}
+
+	dfs_post_order() {
+		return this.traverse_post_order(this.root, []);
+	}
+
+	dfs_pre_order() {
+		return this.traverse_pre_order(this.root, []);
+	}
+
+	traverse_in_order(node, list) {
+		console.log(node.value);
+
+		if (node.left) {
+			this.traverse_in_order(node.left, list);
+		}
+		list.push(node.value);
+		if (node.right) {
+			this.traverse_in_order(node.right, list);
+		}
+		return list;
+	}
+
+	traverse_post_order(node, list) {
+		console.log(node.value);
+
+		if (node.left) {
+			this.traverse_post_order(node.left, list);
+		}
+		if (node.right) {
+			this.traverse_post_order(node.right, list);
+		}
+		list.push(node.value);
+		return list;
+	}
+
+	traverse_pre_order(node, list) {
+		console.log(node.value);
+		list.push(node.value);
+		if (node.left) {
+			this.traverse_pre_order(node.left, list);
+		}
+		if (node.right) {
+			this.traverse_pre_order(node.right, list);
+		}
+
+		return list;
+	}
+
+	true_bst() {
+		return this.is_true_bst(this.root, this.root.value);
+	}
+
+	is_true_bst(node, root_value) {
+		if (node.left) {
+			if (!(node.left.value < root_value)) {
+				return false;
+			}
+			return this.is_true_bst(node.left, root_value);
+		}
+		if (node.right) {
+			if (!(node.right.value > root_value)) {
+				return false;
+			}
+			return this.is_true_bst(node.right);
+		}
+		return true;
+	}
 }
 
 const bst1 = new BST();
@@ -150,6 +258,16 @@ bst1.insert(1);
 bst1.insert(6);
 bst1.insert(15);
 bst1.insert(170);
+console.log('BFS');
+console.log(bst1.bfs());
+console.log('recursive');
+console.log(bst1.bfs_recursive([bst1.root], []));
+console.log('DFS');
+console.log(bst1.dfs_in_order());
+console.log(bst1.dfs_post_order());
+console.log(bst1.dfs_pre_order());
+console.log('TRUE BST');
+console.log(bst1.true_bst());
 
 console.log(JSON.stringify(bst1.root));
 
@@ -162,7 +280,14 @@ function traverse(node) {
 
 console.log('\n Found node: ', bst1.lookup(1));
 console.log('\n Found node: ', bst1.lookup(170));
-console.log('\n Found node: ', bst1.lookup(10));
+console.log('\n Found node: ', bst1.lookup(4));
 
-bst1.remove(6);
-console.log(JSON.stringify(bst1.root));
+// bst1.remove(6);
+// console.log(JSON.stringify(bst1.root));
+
+// function traverse(node) {
+// 	const tree = { value: node.value };
+// 	tree.left = node.left === null ? null : traverse(node.left);
+// 	tree.right = node.right === null ? null : traverse(node.right);
+// 	return tree;
+// }
